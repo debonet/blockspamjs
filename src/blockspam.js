5eb157a28893c5894736cd28d1f425adbd510189
Jeremy De Bonet <jeremy@debonet.com>
2014-10-09 13:41:10 -0400

    add auto-whitelisting

--- src/blockspam.js
+++ src/blockspam.js
@@ -1,21 +1,37 @@
+var D = console.log;
 
 // ---------------------------------------------------------------------------
-var reEmail = /[@\.]/;
-var reLetters = /[A-Za-z]/;
+var reEmail = /@/;
+var reNonLetters = /[^-A-Za-z\.]/;
 var fvsExcludeNonWords = function(vs){
-	return vs.filter(function(s){
+	var vsOut = [];
+
+	vs.map(function(s){
+		if (s.match("http[s]?://")){
+			var s = (s.replace(/^.*:\/\//,'')).replace(/\/.*/,'');
+			return vsOut.push(s);
+		}
+
 		var c = s.length;
 		if (c<4){
 			return false;
 		}
-		if (!s.match(reLetters)){
+
+		if (s.match(reEmail)){
+			s= s.replace(/.*</,'').replace(/>.*/);
+			return vsOut.push(s);
+		}
+
+		if (s.match(reNonLetters)){
 			return false;
 		}
-		if (s.match(reEmail)){
-			return true;
+
+
+		if (c<12){
+			return vsOut.push(s);
 		}
-		return c<12;
 	});
+	return vsOut;
 };
 
 
@@ -48,35 +64,99 @@ var fvsUnique = function(vs){
 	return vsOut;
 };
 
-
 // ---------------------------------------------------------------------------
-var fTrackStats = function(vs,bHam,stat){
-	var av = stat.av;
-	vs.forEach(function(s){
-		// 0 = cHam
-		// 1 = cSeen
-		// 2 = nLastSeen
-		if (!(s in av)){
-			av[s] = [0,0,0];
+var fvrForSender = function(aHeader, stat, bCreate){
+	var sSender = aHeader["From"] || aHeader["Return-Path"];
+	if (sSender && sSender.length){
+		sSender = sSender.replace(/^.*</,'').replace(/>.*$/,'');
+		sSender = sSender.trim();
+		stat.avrSender = stat.avrSender || {};
+		if (!(sSender in stat.avrSender)){
+			if (bCreate){
+				stat.avrSender[sSender] = [0,0];
+			}
+			else{
+				return false;
+			}
 		}
 
-		if (bHam){
-			av[s][0]++;
-		}
-		av[s][1]++;
-		av[s][2] = stat.nGeneration;
-	});
+		return stat.avrSender[sSender];
+	}
 };
 
 // ---------------------------------------------------------------------------
+var fTrackSenderStat = function(aHeader, rScore, stat){
+	var vr = fvrForSender(aHeader, stat, rScore < 0.4);
+	if (vr){
+		vr[0] += 1-rScore;
+		vr[1] += rScore;
+	}
+};
+
+// ---------------------------------------------------------------------------
+var fTrackMessageStat = function(vsMessage, bHam, stat){
+	var avc = stat.avcBody || {};
+	vsMessage.forEach(function(s){
+		// 0 = cHam
+		// 1 = cSeen
+		// 2 = nLastSeen
+		if (!(s in avc)){
+			avc[s] = [0,0,0];
+		}
+
+		if (bHam){
+			avc[s][0]++;
+		}
+		avc[s][1]++;
+		avc[s][2] = stat.nGeneration;
+	});
+}
+// ---------------------------------------------------------------------------
+var fTrackStats = function(aHeader, vsMessage, bHam, stat){
+	fTrackSenderStat(aHeader, bHam?0:1, stat);
+	fTrackMessageStat(vsMessage, bHam, stat);
+};
+
+// ---------------------------------------------------------------------------
+var frEvaluateMessage = function(vsMessage, stat){
+	var avc = stat.avcBody;
+
+	var rT = 0;
+	var c = 0;
+
+	vsMessage.forEach(function(s){
+		if (s in avc){
+			rT += (avc[s][0] / avc[s][1]);
+			c++;
+		}
+	});
+
+
+	return c ? 1 - (rT/c) : 0.5;
+};
+
+// ---------------------------------------------------------------------------
+var frEvaluateSender = function(aHeader, stat){
+	var vr = fvrForSender(aHeader, stat, false);
+	if (vr){
+		if (vr[0] > 2 || vr[1]){
+			return (vr[1]/(vr[0] + vr[1]));
+		}
+	}
+	return false;
+};
+
+
+
+// ---------------------------------------------------------------------------
 var fPurgeRarities = function(stat, rThreshold){
-	var av = stat.av;
+	var avc = stat.avcBody;
 	var c=0;
-	for (s in av){
-		if (av.hasOwnProperty(s)){
-			var cAge = stat.nGeneration - av[s][2];
-			if (av[s][1] / cAge < rThreshold){
-				delete av[s];
+	for (s in avc){
+		if (avc.hasOwnProperty(s)){
+			var cAge = stat.nGeneration - avc[s][2];
+			if (avc[s][1] / cAge < rThreshold){
+				delete avc[s];
 			}
 			else{
 				c++;
@@ -87,35 +167,57 @@ var fPurgeRarities = function(stat, rThreshold){
 };
 
 
-
-
 // ---------------------------------------------------------------------------
+var faHeaderProcess = function(s){
+	vs = s.split('\n');
+	
+	var bInHeader = true;
 
-/*
-var fProcessHeader = function(){
-var bInHeader = true;
-	if (bInHeader){
-		var vs = s.split('\n');
-		vs.some(function(sLine){
-			if (sLine.length===0){
-				bInHeader = false;
-				return true;
+	var aHeader = {};
+
+	var sVar;
+	var sVal="";
+
+	vs.some(function(sLine){
+		if (sLine.length===0){
+			return true;
+		}
+
+		if (sVar){
+			var ch0 = sLine.charAt(0);
+			if (ch0 === ' ' || ch0 === "\t"){
+				sVal = sVal + " " + sLine.trim();
+				return;
 			}
 
-			// process headers here
-		});
-  }
+			aHeader[sVar]=sVal;
+			sVar=undefined;
+			sVal="";
+		}
+
+		var vsLine = sLine.split(':');
+		sVar = vsLine[0];
+		
+		sVal = sLine.substring(sVar.length+1).trim();
+
+	});
+
+	if (sVar){
+		aHeader[sVar]=sVal;
+	}		
+	
+	return aHeader;
+
 };
-*/
 
 
 
 // ---------------------------------------------------------------------------
-var fvsProcessMessage = function(s){
+var fvsMessageProcess = function(s){
 	s=s.replace(/>/g,"> ");
 	s=s.replace(/</g," <");
 	s=s.replace(/[;=,]/g," ");
-	var vs=s.split(/[\s]+/);
+	var vs=s.split(/[\s"']+/);
 	vs = fvsExcludeNonWords(vs);
 	//	vs = fvsPairs(vs);
 	vs = fvsUnique(vs);
@@ -125,40 +227,7 @@ var fvsProcessMessage = function(s){
 
 
 // ---------------------------------------------------------------------------
-var frEvaluate = function(vs,stat){
-	var av = stat.av;
-
-	var rT = 0;
-	var c = 0;
-
-	vs.forEach(function(s){
-		if (s in av){
-			rT += (av[s][0] / av[s][1]);
-			c++;
-		}
-	});
-
-
-	return c ? 1 - (rT/c) : 0.5;
-};
-
-
-
-// ---------------------------------------------------------------------------
-var fsRepeat = function(s,c){
-	var sOut = "";
-	for (var n=0; n<c; n++){
-		sOut += s;
-	}
-	return sOut;
-};
-
-// ---------------------------------------------------------------------------
-var fHandleEmailStream = function(stream, bHam, bSpam, stat, fCallback){
-
-	if (bHam && bSpam){
-		return fCallback("can't be ham and spam!");
-	}
+var fEvaluateEmail = function(stream, bGuess, stat, fCallback){
 
 	var sMessage = "";
 
@@ -173,36 +242,79 @@ var fHandleEmailStream = function(stream, bHam, bSpam, stat, fCallback){
 	});
 
 
-	if (bHam || bSpam){
-		// --------------------
-		stream.on('end', function() {
-			fCallback(null,"message added as " + (bHam?"ham":"spam"), sMessage);
-			var vs = fvsProcessMessage(sMessage);
-			fTrackStats(vs, bHam, stat);
-			if ((stat.nGeneration%10) === 0){
-				fPurgeRarities(stat, 0.0005);
-			}
-		});
-	}
-	else{
-		stream.on('end', function() {
-			var vs = fvsProcessMessage(sMessage);
-			var r = frEvaluate(vs, stat);
-			var nScore = Math.floor(r*100);
-			var sScoreStar = fsRepeat('*', Math.floor(r*20)+1);
-			sMessage = (
-				""
-					+ "X-BlockSpam-Score: " + nScore + "\n" 
-					+ "X-BlockSpam-Stars: " + sScoreStar + "\n" 
-					+ sMessage
-					+ "\n"
-					+ "-----------------------\n"
-					+ "BlockSpam-Score: "  + nScore + "\n"
-					+ "\n"
-			);
-			fCallback(null, "score: " + nScore, sMessage);
-		});
-	}
+	stream.on('end', function() {
+		var aHeader   = faHeaderProcess(sMessage);
+		var vsMessage = fvsMessageProcess(sMessage);
+		var rBody  = null;
+		var rSender   = frEvaluateSender(aHeader,stat) 
+		
+		var bSenderGood = rSender!==false && (rSender > .75 || rSender<.25);
+
+		if (!bSenderGood || bGuess){
+			rBody = frEvaluateMessage(vsMessage, stat);
+		}
+
+		var rScore = bSenderGood ? rSender : rBody;
+
+		if (bGuess){
+			fTrackSenderStat(aHeader, 1-rScore, stat);
+		}
+
+		var nScore = Math.floor(rScore * 100);
+		var nSender = Math.floor(rSender * 100);
+		var nBody = Math.floor(rBody * 100);
+
+		var sScore  = "X-BlockSpam-Score: " + nScore + "\n";
+		var sSender = (
+			rSender !== false 
+				?	"X-BlockSpam-Sender: " + nSender + "\n" 
+				: ""
+		);
+
+		var sBody = (
+			rBody !== null
+				?	"X-BlockSpam-Body: " + nBody + "\n" 
+				: ""
+		);
+
+		sMessage = (sScore + sSender + sBody	+ sMessage);
+		fCallback(
+			null, 
+			"score: " + nScore + "/" + nSender + "/" + nBody, 
+			sMessage
+		);
+	});
+
+};
+
+
+// ---------------------------------------------------------------------------
+var fTrainOnEmail = function(stream, bHam, stat, fCallback){
+
+	var sMessage = "";
+
+	// --------------------
+	stream.on('readable', function() {
+		var s = stream.read();
+		if (s === null) {
+			return;
+		}
+
+		sMessage += s;
+	});
+
+
+	// --------------------
+	stream.on('end', function() {
+		var aHeader   = faHeaderProcess(sMessage);
+		var vsMessage = fvsMessageProcess(sMessage);
+
+		fTrackStats(aHeader,vsMessage, bHam, stat);
+		if ((stat.nGeneration%10) === 0){
+			fPurgeRarities(stat, 0.0005);
+		}
+		fCallback(null,"message added as " + (bHam?"ham":"spam"), sMessage);
+	});
 };
 
 
@@ -216,7 +328,8 @@ var fLoadStats = function(sfl, fCallback){
 		catch(e){
 			stat = {
 				nGeneration : -1,
-				av : {}
+				avcBody    : {},
+				avrSender   : {}
 			};
 		}
 
@@ -233,40 +346,76 @@ var fWriteStats = function(sfl, stat, fCallback){
 
 
 // ---------------------------------------------------------------------------
-var fDaemonMode = function(sflDatabase, nPort){
+var fDaemonMode = function(aOptions){
+	var sflDatabase = aOptions["database"];
+	var nPort       = aOptions["port"]
+
 	console.log("Daemon mode");
 	fLoadStats(sflDatabase, function(err, stat){
 		nsHttp.createServer(function (req, res) {
-			var bHam  = (req.url === "/ham" );
-			var bSpam = (req.url === "/spam");
+			var bHam   = (req.url === "/ham" );
+			var bSpam  = (req.url === "/spam");
+			var bGuess = (req.url === "/guess");
+
+			var tm = new Date().getTime();
+
 			var bTraining = bHam || bSpam;
-			if (bTraining){
+			if (bTraining || bGuess){
 				stat.nGeneration++;
 			}
-			var tm = new Date().getTime();
-			fHandleEmailStream(req, bHam, bSpam, stat, function(err,s, sMessage){
-				console.log("processed", s, ((new Date().getTime())-tm)/1000);
-				res.writeHead(200, {'Content-Type': 'text/plain'});
-				res.end(bTraining ? s : sMessage);
-				if (bTraining && (stat.nGeneration % 50) === 0){
-					fWriteStats(sflDatabase, stat);
-				}					
-			});
+
+			if (bTraining){
+				fTrainOnEmail(req, bHam, stat, function(err,s){
+					console.log("processed", s, ((new Date().getTime())-tm)/1000);
+					res.writeHead(200, {'Content-Type': 'text/plain'});
+					res.end(s);
+					if (stat.nGeneration % 50 === 0){
+						fWriteStats(sflDatabase, stat);
+					}					
+				});
+			}
+			else{
+				fEvaluateEmail(req, bGuess, stat, function(err,s, sMessage){
+					console.log("processed", s, ((new Date().getTime())-tm)/1000);
+					res.writeHead(200, {'Content-Type': 'text/plain'});
+					res.end( sMessage);
+					if (bGuess && (stat.nGeneration % 50) === 0){
+						fWriteStats(sflDatabase, stat);
+					}					
+				});
+
+			}
+
 		}).listen(nPort);
 	});
 };
 
 // ---------------------------------------------------------------------------
-var fClientMode = function(
-	sflMessage, bHam, bSpam, sflDatabase, sServer, nPort
-){
-	var sPath = bHam? "/ham" : (bSpam ? "/spam" : "/score");
+var fClientMode = function(aOptions){
+	var nPort       = aOptions["port"]
+	var sServer     = aOptions["server"];
+	var bHam        = aOptions["ham"];
+	var bSpam       = aOptions["spam"];
+	var bGuess      = aOptions["guess"];
+	var sflMessage  = aOptions["message"];
+
+	var sIndex = "" + bHam + ":" + bSpam + ":" + bGuess;
+
+	var asPath = {
+		"true:false:false" : "/ham",
+		"false:true:false" : "/spam",
+		"false:false:true" : "/guess",
+	};
+
+	var sPath = asPath[sIndex] || "/evaluate";
+
 	var aOptions = {
 		hostname : sServer,
 		port     : nPort,
 		method   : "POST",
 		path     : sPath,
 	};
+
 	var req=nsHttp.request(aOptions, function(res){
 		res.setEncoding('utf8');
 		res.on('data', function (s) {
@@ -274,7 +423,7 @@ var fClientMode = function(
 		}); 
 	});
 	req.on('error', function(){
-		console.log("COULD NOT OPEN");
+		console.log("COULD NOT REACH DAEMON");
 		process.exit(1);
 	});
 
@@ -283,21 +432,34 @@ var fClientMode = function(
 };
 
 // ---------------------------------------------------------------------------
-var fStandaloneMode = function(sflMessage, bHam, bSpam, sflDatabase){
+var fStandaloneMode = function(aOptions){
+	var sflDatabase = aOptions["database"];
+	var bHam        = aOptions["ham"];
+	var bSpam       = aOptions["spam"];
+	var bGuess      = aOptions["guess"];
+	var sflMessage  = aOptions["message"];
+
 	fLoadStats(sflDatabase, function(err, stat){
 		var stream = nsFs.createReadStream(sflMessage);
 		var bTraining = bHam || bSpam;
-		if (bTraining){
+		if (bTraining || bGuess){
 			stat.nGeneration++;
 		}
-		fHandleEmailStream(
-			stream, bHam, bSpam, stat, function(err,s, sMessage){
-				console.log((bHam || bSpam)?s:sMessage);
-				if (bTraining){
+
+		if (bTraining){
+			fTrainOnEmail(stream, bHam, stat, function(err,s){
+				console.log(s);
+				fWriteStats(sflDatabase, stat);
+			});
+		}
+		else{
+			fEvaluateEmail(stream, bGuess, stat, function(err,s, sMessage){
+				console.log(sMessage);
+				if (bGuess){
 					fWriteStats(sflDatabase, stat);
-				}
-			}
-		);
+				}					
+			});
+		}
 	});
 };
 
@@ -331,6 +493,11 @@ var fMain = function(){
 		.default('s',false)
 		.alias('s', 'spam')
 		.describe('s', "Train using message as spam")
+
+		.boolean('g')
+		.default('g',false)
+		.alias('g', 'guess')
+		.describe('g', "Guess on spam status and train sender with result")
 
 		.string('d')
 		.default('d',false)
@@ -367,25 +534,20 @@ var fMain = function(){
 		yargs.showHelp();
 		process.exit();
 	}
+	aArg["message"] = aArg._[0] || "/dev/stdin";
 
-	var sflDatabase = aArg["database"];
-	var nPort       = aArg["port"]
-	var sServer     = aArg["server"];
-	var bHam        = aArg["ham"];
-	var bSpam       = aArg["spam"];
-	var sflMessage  = aArg._[0] || "/dev/stdin";
 
 	// daemon mode
 	if (aArg["daemon"]){
-		fDaemonMode(sflDatabase, nPort);
+		fDaemonMode(aArg);
 	}
 	// client mode
 	else if (aArg["client"]){
-		fClientMode(sflMessage, bHam, bSpam, sflDatabase, sServer, nPort);
+		fClientMode(aArg);
 	}
 	// standalone mode
 	else{ 
-		fStandaloneMode(sflMessage, bHam, bSpam, sflDatabase);
+		fStandaloneMode(aArg);
 	}
 
 }
